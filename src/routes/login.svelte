db6a78ef0745dba4b18de81a564e47e39cee17c0
rboddy <avidryanb@gmail.com>
2022-03-06 07:00:20 -0500

    Suppressing client side errors for user enum

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -17,7 +17,6 @@
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(`${errorCode}: ${errorMessage}`);
       });
   }
 </script>
