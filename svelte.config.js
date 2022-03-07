be55e39b72e9bb41d137d2da66d96e699d9c6769
rboddy <avidryanb@gmail.com>
2022-03-06 23:59:01 -0500

    Adding the node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
