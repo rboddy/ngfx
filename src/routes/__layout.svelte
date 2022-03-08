4a5f500b80d1a1dad875837718933ac1b355eb0a
rboddy <avidryanb@gmail.com>
2022-03-08 11:39:37 -0500

    Fixing some auth logic, still have to resolve

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,19 @@
 <script>
   import Navbar from "$lib/components/layout/navbar.svelte";
   import Header from "$lib/components/layout/header.svelte";
-  import App from "./fb";
+  import { initializeApp } from "firebase/app";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCk_G2jEhyeZrIqdX7L2U8oUfq7S2januM",
+    authDomain: "ngfx-dd48b.firebaseapp.com",
+    projectId: "ngfx-dd48b",
+    storageBucket: "ngfx-dd48b.appspot.com",
+    messagingSenderId: "599552784740",
+    appId: "1:599552784740:web:7caf3d8e940d6a124b6271",
+  };
+
+  initializeApp(firebaseConfig);
 </script>
 
 <Navbar />
